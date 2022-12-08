2962c88a62f357f5eae2a72db6411178940669a4
Masanari Makino <masanari773@gmail.com>
2022-12-08 13:52:39 +0900

    View prototype

--- src/views/ClassroomView.vue
+++ src/views/ClassroomView.vue
@@ -1,18 +1,206 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <v-layout>
+        <v-app-bar :elevation="2">
+            <template v-slot:prepend>
+                <v-app-bar-nav-icon></v-app-bar-nav-icon>
+            </template>
+
+            <v-app-bar-title>情報工学概論1</v-app-bar-title>
+        </v-app-bar>
+        <v-main class="ml-4 mr-4">
+            <div v-for="pair in pairsOnSelected" :key="pair">
+                <v-card
+                  class="mx-auto mb-2 mt-4"
+                  color="pair.color"
+                  max-width="400"
+                  prepend-icon="mdi-twitter"
+                  title="Funeeet"
+                >
+                  <template v-slot:prepend>
+                    <v-icon size="x-large"></v-icon>
+                  </template>
+
+                  <v-card-text class="text-h5 py-2">
+                        {{pair.funeeet.body}}
+                  </v-card-text>
+
+                  <v-card-actions>
+                    <v-list-item class="w-100">
+                      <template v-slot:prepend>
+                        <v-avatar
+                          color="grey-darken-3"
+                          image="https://avataaars.io/?avatarStyle=Transparent&topType=ShortHairShortCurly&accessoriesType=Prescription02&hairColor=Black&facialHairType=Blank&clotheType=Hoodie&clotheColor=White&eyeType=Default&eyebrowType=DefaultNatural&mouthType=Default&skinColor=Light"
+                        ></v-avatar>
+                      </template>
+
+                      <v-list-item-title>{{pair.funeeet.name}}</v-list-item-title>
+
+
+                      <template v-slot:append>
+                        <div class="justify-self-end">
+                          <v-icon class="mr-1" icon="mdi-thumb-up"></v-icon>
+                          <span class="subheading mr-2">256</span>
+                        </div>
+                      </template>
+                    </v-list-item>
+                  </v-card-actions>
+                </v-card>
+            </div>
+            <div class="mx-auto" style="display: flex; justify-content: flex-end; max-width: 400px;">
+                <v-btn icon="mdi-plus" class="mr-4" style="position: fixed; bottom:0;" color="info">
+                    <v-dialog
+                        v-model="dialog"
+                        activator="parent"
+                        style="max-width: 400px;"
+                    >
+                        <v-card >
+                            <v-textarea
+                                clearable
+                                clear-icon="mdi-close-circle"
+                                label="Funeeet yourself!"
+                            >
+                            </v-textarea>
+
+                            <v-card-actions>
+                                <v-btn color="primary" block @click="onFuneeet">Funeeet</v-btn>
+                            </v-card-actions>
+                        </v-card>
+                    </v-dialog>
+
+                </v-btn>
+            </div>
+            
+            
+            <div class="text-center">
+                <v-pagination
+                  v-model="page"
+                  :length="pageNum"
+                ></v-pagination>
+            </div>
+        </v-main>
+    </v-layout>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
+    name: 'ClassroomView',
+    components: {
+    },
+    data: () => { return {
+            funeeets: [
+                {
+                    "id": 1,
+                    "body": "この授業むずすぎ",
+                    "name": "飯島里穂",
+                    "likes": 250
+                },
+                {
+                    "id": 2,
+                    "body": "この授業むずすぎ",
+                    "name": "飯島里穂",
+                    "likes": 250
+                },
+                {
+                    "id": 3,
+                    "body": "この授業むずすぎ",
+                    "name": "飯島里穂",
+                    "likes": 250
+                },
+                {
+                    "id": 4,
+                    "body": "この授業むずすぎ",
+                    "name": "飯島里穂",
+                    "likes": 250
+                },
+                {
+                    "id": 5,
+                    "body": "この授業むずすぎ",
+                    "name": "飯島里穂",
+                    "likes": 250
+                },
+                {
+                    "id": 6,
+                    "body": "この授業むずすぎ",
+                    "name": "飯島里穂",
+                    "likes": 250
+                },
+                {
+                    "id": 7,
+                    "body": "この授業むずすぎ",
+                    "name": "飯島里穂",
+                    "likes": 250
+                },
+                {
+                    "id": 8,
+                    "body": "この授業むずすぎ",
+                    "name": "飯島里穂",
+                    "likes": 250
+                },
+                {
+                    "id": 9,
+                    "body": "この授業むずすぎ",
+                    "name": "飯島里穂",
+                    "likes": 250
+                },
+                {
+                    "id": 10,
+                    "body": "この授業むずすぎ",
+                    "name": "飯島里穂",
+                    "likes": 250
+                },
+                {
+                    "id": 11,
+                    "body": "この授業むずすぎ",
+                    "name": "飯島里穂",
+                    "likes": 250
+                },
+            ],
+            colors: [],
+            page: 1,
+            page_size: 10,
+            dialog: false,
+            currentUserName: "飯島 美穂"
+        }
+    },
+    computed: {
+        funeeetColorPairs() {
+            return this.funeeets.map((funeeet, i) => {
+                return {
+                    funeeet: funeeet,
+                    color: this.colors[i]
+                }
+            })
+        },
+        pairsOnSelected() {
+            if (this.page == this.pageNum) {
+                return this.funeeetColorPairs.slice((this.page - 1) * this.page_size);
+            }
+            return this.funeeetColorPairs.slice((this.page - 1) * this.page_size, this.page * this.page_size)
+        },
+        pageNum() {
+            return Math.ceil(this.funeeets.length / this.page_size);
+        }
+    },
+    mounted: function() {
+        for (let i = 0; i < this.funeeets.length; i++) {
+            this.colors.push('#'+(Math.random()*0xFFFFFF<<0).toString(16));
+        }
+    },
+    methods: {
+        scrollToEnd() {
+            this.$nextTick(() => {
+                const funeeet_button = document.getElementById('funeeet_box');
+                if (!funeeet_button) return;
+                funeeet_button.scrollTop = funeeet_button.scrollHeight;
+            })
+        },
+        onFuneeet() {
+        }
+    },
+    updated() {
+        this.scrollToEnd();
+    }
 }
 </script>
